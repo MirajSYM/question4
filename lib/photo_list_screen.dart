4572558bfbe237cff74118a3233a25ed99c038e0
MirajSYM <mirajahmedsymanto@gmail.com>
2020-07-07 13:44:19 +0800

    Final version

--- lib/photo_list_screen.dart
+++ lib/photo_list_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'photo.dart';
+import 'photo_screen.dart';
+import 'rest_service.dart';
 
 class PhotoListScreen extends StatefulWidget {
   @override
@@ -8,39 +10,56 @@ class PhotoListScreen extends StatefulWidget {
 }
 
 class _PhotoListScreenState extends State<PhotoListScreen> {
+  RestService service = RestService();
+  List<Photo> photos;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Main screen')),
-    );
+    return FutureBuilder<List<Photo>>(
+        future: service.getAllPhotos(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            photos = snapshot.data;
+            return _buildMainScreen();
+          }
+          return _buildFetchingDataScreen();
+        });
   }
 
   Widget _buildMainScreen() {
     return Scaffold(
       body: ListView.separated(
-        itemCount: 0,
+        itemCount: photos.length,
         separatorBuilder: (context, index) => Divider(
           color: Colors.blueGrey,
         ),
         itemBuilder: (context, index) {
           return ListTile(
             title: SizedBox(
-                height: 80,
+                height: 120,
                 child: InkWell(
-                  onTap: () {},
+                  child: Image.network(photos[index].thumbUrl),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              PhotoScreen(url: photos[index].photoUrl)),
+                    );
+                  },
                 )),
-            trailing: null,
+            trailing: _buildThumbButtons(photos[index]),
           );
         },
       ),
     );
   }
 
-  Widget _buildThumbButtons([Photo photo = null]) {
+  Widget _buildThumbButtons([Photo photo]) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        Text('likes',
+        Text(photo.like.toString(),
             style: TextStyle(
                 color: Colors.green,
                 fontWeight: FontWeight.bold,
@@ -50,17 +69,42 @@ class _PhotoListScreenState extends State<PhotoListScreen> {
               Icons.thumb_up,
               color: Colors.green,
             ),
-            onPressed: () {}),
+            onPressed: () async {
+              Photo update = await service.votePhoto(id: photo.id, like: true);
+              setState(() {
+                photo.like = update.like;
+              });
+            }),
         IconButton(
             icon: Icon(
               Icons.thumb_down,
               color: Colors.red,
             ),
-            onPressed: () {}),
-        Text('dislike',
+            onPressed: () async {
+              Photo update = await service.votePhoto(id: photo.id, like: false);
+              setState(() {
+                photo.dislike = update.dislike;
+              });
+            }),
+        Text(photo.dislike.toString(),
             style: TextStyle(
                 color: Colors.red, fontWeight: FontWeight.bold, fontSize: 15)),
       ],
+    );
+  }
+
+  Scaffold _buildFetchingDataScreen() {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircularProgressIndicator(),
+            SizedBox(height: 50),
+            Text('Fetching todo... Please wait'),
+          ],
+        ),
+      ),
     );
   }
 }
